ed77b5f64a0ee2757048b20b65e3183bf13de7f3
zhimen <736099435@qq.com>
2018-04-25 16:57:53 +0800

    List user replies

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,21 +3,9 @@
 use Faker\Generator as Faker;
 use Carbon\Carbon;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(App\Models\User::class, function (Faker $faker) {
     static $password;
     $now = Carbon::now()->toDateTimeString();
-
 
     return [
         'name' => $faker->name,
